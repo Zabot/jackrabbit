d9809f16fefff6714c34b58c6966e010d908905e
Zach Anderson <zach.inbox@gmail.com>
2022-05-08 21:22:27 -0500

    Move handlers into functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rouille::Response;
-
 use askama::Template;
+use rouille::Request;
+use rouille::Response;
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -12,29 +12,30 @@ struct PluginTemplate<'a>{
     host: &'a str,
 }
 
+fn handle_index(request: &Request) -> Response {
+    let index = IndexTemplate{};
+    Response::html(index.render().unwrap())
+}
+
+fn handle_plugin(request: &Request) -> Response {
+    match request.header("Host") {
+        None => Response::text("Missing Host").with_status_code(400),
+        Some(host) => {
+            let search = PluginTemplate{
+                host: host,
+            };
+            let body = search.render().unwrap().into_bytes();
+            Response::from_data("text/xml", body)
+        }
+    }
+}
+
 fn main() {
     println!("Jackrabbit started");
     rouille::start_server("0.0.0.0:8080", move |request| {
 		rouille::router!(request,
-			(GET) ["/"] => {
-				let index = IndexTemplate{};
-                Response::html(index.render().unwrap())
-			},
-
-			(GET) ["/opensearch.xml"] => {
-                let maybe_host = request.header("Host");
-                match maybe_host {
-                    None => Response::text("Missing Host").with_status_code(400),
-                    Some(host) => {
-                        let search = PluginTemplate{
-                            host: host,
-                        };
-                        let body = search.render().unwrap().into_bytes();
-                        Response::from_data("text/xml", body)
-                    }
-                }
-			},
-
+			(GET) ["/"] => handle_index(&request),
+			(GET) ["/opensearch.xml"] => handle_plugin(&request),
             _ => Response::text("Not found").with_status_code(404)
 		)
     });
