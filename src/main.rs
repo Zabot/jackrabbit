9cb31b4fac11383393b9372008322d80cd5af0ef
Zach Anderson <zach.inbox@gmail.com>
2022-05-08 21:55:33 -0500

    Handle search

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs;
+use std::collections::HashMap;
+
 use askama::Template;
 use rouille::Request;
 use rouille::Response;
+use serde_derive::Deserialize;
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -12,7 +16,12 @@ struct PluginTemplate<'a>{
     host: &'a str,
 }
 
-fn handle_index(request: &Request) -> Response {
+#[derive(Deserialize)]
+struct Config {
+    bookmarks: HashMap<String, String>,
+}
+
+fn handle_index(_request: &Request) -> Response {
     let index = IndexTemplate{};
     Response::html(index.render().unwrap())
 }
@@ -30,12 +39,41 @@ fn handle_plugin(request: &Request) -> Response {
     }
 }
 
+fn handle_search(bookmarks: &HashMap<String, String>, request: &Request) -> Response {
+    match request.get_param("q") {
+        None => Response::text("Missing query").with_status_code(400),
+        Some(query) => {
+            for (key, value) in bookmarks {
+                // If key is perfect equality, do a permanent redirect
+                if key.eq(&query) {
+                    return Response::redirect_301(value.clone());
+                }
+
+                // Append a space and check for prefix
+                let prefix = format!("{} ", key);
+                match query.strip_prefix(&prefix) {
+                    Some(m) => {
+                        let url = value.replace("{}", m.trim());
+                        return Response::redirect_302(url);
+                    }
+                    None => {}
+                }
+            }
+            Response::text(query).with_status_code(200)
+        }
+    }
+}
+
 fn main() {
-    println!("Jackrabbit started");
+    println!("Loading bookmarks");
+    let config_contents = fs::read_to_string("jackrabbit.toml").expect("Failed to read config");
+    let config: Config = toml::from_str(&config_contents).unwrap();
+
     rouille::start_server("0.0.0.0:8080", move |request| {
 		rouille::router!(request,
 			(GET) ["/"] => handle_index(&request),
 			(GET) ["/opensearch.xml"] => handle_plugin(&request),
+            (GET) ["/search"] => handle_search(&config.bookmarks, &request),
             _ => Response::text("Not found").with_status_code(404)
 		)
     });
